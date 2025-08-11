a088f7b4a931de89e5c54a4d88a0935f37e79b83
desktop <zhouyi_207@foxmail.com>
2025-08-11 16:44:44 +0800

    update: access, refresh token

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -2,11 +2,13 @@ import { Navigate, useLocation } from "react-router";
 import { useEffect, useState } from "react";
 import { validateToken } from "@/api/auth"; // 用封装好的 API
 import React from "react";
+import { useUserInfo } from "@/stores/useUserInfo";
 
 export function RequireAuth({ children }: { children: React.ReactNode }) {
   const location = useLocation();
   const [checking, setChecking] = useState(true);
   const [valid, setValid] = useState(false);
+  const { setData } = useUserInfo();
 
   useEffect(() => {
     const cached = sessionStorage.getItem("token_valid");
@@ -17,8 +19,9 @@ export function RequireAuth({ children }: { children: React.ReactNode }) {
     }
 
     validateToken()
-      .then(() => {
+      .then((res) => {
         setValid(true);
+        setData(res);
         sessionStorage.setItem("token_valid", "true");
       })
       .catch(() => {
