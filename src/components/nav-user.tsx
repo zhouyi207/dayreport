575da078fcc56e27d580eac28ed88fc89aef867e
desktop <zhouyi_207@foxmail.com>
2025-08-11 16:46:12 +0800

    update: sidebarData

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -25,6 +25,7 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
+import { useUserInfo } from "@/stores/useUserInfo";
 
 export function NavUser({
   user,
@@ -36,6 +37,7 @@ export function NavUser({
   };
 }) {
   const { isMobile } = useSidebar();
+  const { data } = useUserInfo();
 
   return (
     <SidebarMenu>
@@ -51,8 +53,8 @@ export function NavUser({
                 <AvatarFallback className="rounded-lg">CN</AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
+                <span className="truncate font-medium">{data?.name}</span>
+                <span className="truncate text-xs">{data?.username}</span>
               </div>
               <ChevronsUpDown className="ml-auto size-4" />
             </SidebarMenuButton>
@@ -70,8 +72,8 @@ export function NavUser({
                   <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
+                  <span className="truncate font-medium">{data?.name}</span>
+                  <span className="truncate text-xs">{data?.username}</span>
                 </div>
               </div>
             </DropdownMenuLabel>
