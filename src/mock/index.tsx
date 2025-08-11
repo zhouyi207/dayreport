c33f440f714027f53916ae092a1a2b5fb4823f52
desktop <zhouyi_207@foxmail.com>
2025-08-11 16:44:13 +0800

    delete: mock function

--- src/mock/index.tsx
+++ src/mock/index.tsx
@@ -5,41 +5,12 @@ import { sidebarData } from "@/data/sidebar";
 
 const mock = new MockAdapter(http, { delayResponse: 500 });
 
-mock.onPost("/auth/login/").reply((config) => {
-  const { username, password } = JSON.parse(config.data);
-  if (username === "test@example.com" && password === "123") {
-    return [
-      200,
-      {
-        access: "mock-access-token",
-        refresh: "mock-refresh-token",
-      },
-    ];
-  } else {
-    return [401, { detail: "用户名或密码错误" }];
-  }
-});
-
-mock.onGet("/auth/validate/").reply((config) => {
-  const authHeader = config.headers?.Authorization;
-  if (authHeader === "Bearer mock-access-token") {
-    return [200, { detail: "Token is valid." }];
-  } else {
-    return [401, { detail: "Token 无效或已过期" }];
-  }
-});
-
 mock.onGet("/users").reply((config) => {
   return [200, sidebarData];
 });
 
-mock.onPost("/login").reply((config) => {
-  const { email, password } = JSON.parse(config.data);
-  if (email === "test@example.com" && password === "123") {
-    return [200, { message: "Login successful" }];
-  }
-  return [401, { message: "Invalid email or password" }];
-});
+
+mock.onAny().passThrough();
 
 export default {
   bootstrap() {
