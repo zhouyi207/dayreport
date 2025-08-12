9638d972cc62976ddac26297cbc4b4f18e4cfcba
desktop <zhouyi_207@foxmail.com>
2025-08-12 14:50:52 +0800

    update: mock

--- src/mock/index.tsx
+++ src/mock/index.tsx
@@ -1,14 +1,7 @@
-// src/mock/index.ts
 import MockAdapter from "axios-mock-adapter";
-import http from "@/lib/http"; // 引入你封装的实例
-import { sidebarData } from "@/data/sidebar";
+import http from "@/lib/http";
 
 const mock = new MockAdapter(http, { delayResponse: 500 });
-
-mock.onGet("/users").reply((config) => {
-  return [200, sidebarData];
-});
-
 
 mock.onAny().passThrough();
 
