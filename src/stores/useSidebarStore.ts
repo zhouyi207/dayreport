4c69183525658e72e844afb9557099a31abf8656
desktop <zhouyi_207@foxmail.com>
2025-08-12 14:49:42 +0800

    add: init

--- src/stores/useSidebarStore.ts
+++ src/stores/useSidebarStore.ts
@@ -1,46 +1,38 @@
 // stores/useSidebarStore.ts
 import { create } from "zustand";
 import { getsiderbar } from "@/api/auth";
-import { persist } from "zustand/middleware";
 import type { Role, SidebarData } from "@/data/sidebar";
 
 interface SidebarState {
   data: SidebarData | null;
   activateRole: Role | undefined;
   setActivateRole: (role: Role) => void;
+  init: () => void;
   fetchSidebar: () => Promise<void>;
 }
 
-export const useSidebarStore = create<SidebarState>()(
-  persist(
-    (set, get) => ({
-      data: null,
-      roles: [],
-      activateRole: undefined,
+export const useSidebarStore = create<SidebarState>()((set, get) => ({
+  data: null,
+  roles: [],
+  activateRole: undefined,
 
-      setActivateRole: (role: Role) => {
-        set({ activateRole: role });
-      },
+  init: async () => {
+    set({ data: null, activateRole: undefined });
+  },
 
-      fetchSidebar: async () => {
-        if (get().data) return;
-        try {
-          const rawData = await getsiderbar();
-          set({
-            data: rawData,
-            activateRole: rawData.roles[0],
-          });
-        } catch (e) {
-          set({ data: null, activateRole: undefined });
-        }
-      },
-    }),
-    {
-      name: "sidebar-storage",
-      partialize: (state) => ({
-        data: state.data,
-        activateRole: state.activateRole,
-      }),
+  setActivateRole: (role: Role) => {
+    set({ activateRole: role });
+  },
+
+  fetchSidebar: async () => {
+    try {
+      const rawData = await getsiderbar();
+      set({
+        data: rawData,
+        activateRole: rawData.roles[0],
+      });
+    } catch (e) {
+      set({ data: null, activateRole: undefined });
     }
-  )
-);
+  },
+}));
