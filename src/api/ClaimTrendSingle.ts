68c9b786208c80835fad5e0c83265e23fbdd4ee7
desktop <zhouyi_207@foxmail.com>
2025-08-21 15:02:55 +0800

    add: abortsignal

--- src/api/ClaimTrendSingle.ts
+++ src/api/ClaimTrendSingle.ts
@@ -1,4 +1,5 @@
 import http from "@/lib/http";
+import type { AxiosRequestConfig } from "axios";
 import type { Selected } from "@/data/select";
 
 export interface DataItem {
@@ -11,7 +12,13 @@ export interface TrendData {
   数据: DataItem[];
 }
 
-export async function getclaimTrendSingle(data: Selected): Promise<TrendData> {
-  const res = await http.post("/api/claim-trend-single/data", data);
+export async function getclaimTrendSingle(
+  data: Selected,
+  signal?: AbortSignal
+): Promise<TrendData> {
+  const config: AxiosRequestConfig = {
+    signal,
+  };
+  const res = await http.post("/api/claim-trend-single/data", data, config);
   return res.data;
 }
