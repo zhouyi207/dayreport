15e9170ad8ad1a9e2c5b95d98abfd6c5a01d9d13
desktop <zhouyi_207@foxmail.com>
2025-08-21 15:04:22 +0800

    delete: useless component

--- src/app/test.tsx
+++ src/app/test.tsx
@@ -1,8 +1,4 @@
 import { MultiHeaderTable } from "@/components/custom-ui/table";
-import { ChartAreaInteractive } from "@/components/custom-ui/chart-area";
-import { chartAreaInteractiveParams } from "@/data/chartArea";
-import { EChartAreaInteractive } from "@/components/echart/AreaChart";
-import { BubbleChart } from "@/components/echart/BubbleChart";
 import { MapChart } from "@/components/echart/MapChat";
 
 export default function Test() {
@@ -11,14 +7,7 @@ export default function Test() {
       <div className="h-200">
         <MapChart />
       </div>
-      <BubbleChart />
-      <EChartAreaInteractive
-        chartAreaInteractiveParams={chartAreaInteractiveParams}
-      />
       <MultiHeaderTable />
-      <ChartAreaInteractive
-        chartAreaInteractiveParams={chartAreaInteractiveParams}
-      />
     </div>
   );
 }
